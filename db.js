cc286dcc90cba524cb0d3bd4d2d3a4b0b212028b
Yang Bianyue, Brian <brian.yang@merry.com.sg>
2017-07-21 09:17:21 +0800

    Initial Commit

---
+++ db.js
@@ -0,0 +1,43 @@
+var sqlite3 = require('sqlite3').verbose();
+var Promise = require('promise');
+// Instantiate SQLite
+var engine = new sqlite3.Database('localDB.db');
+
+engine.serialize(function () {
+    // Create the key-value table
+    engine.run('CREATE TABLE IF NOT EXISTS KeyValue (id INTEGER PRIMARY KEY AUTOINCREMENT, key TEXT, value BLOB, update_time DATETIME UNIQUE)');
+});
+
+// All DB methods
+var db = {
+    // Get value by key and timestamp (optional)
+    getValueByKey: function (keyIn, timestampIn) {
+        return new Promise(function (fulfill, reject) {
+            query = 'SELECT key, value, update_time FROM KeyValue WHERE key = ?';
+            if (timestampIn && timestampIn !== null)
+                query += ' AND update_time <= ?';
+            query += ' ORDER BY update_time DESC LIMIT 1'
+            engine.get(query, keyIn, timestampIn, function (err, row) {
+                if (err)
+                    reject(err);
+                else if (!row)
+                    reject('No result found');
+                else
+                    fulfill(row);
+            });
+        });
+    },
+    // Insert a new key-value pair, if key exists, replace it
+    insertUpdateEntry: function (keyIn, valueIn, timestampIn) {
+        return new Promise(function (fulfill, reject) {
+            engine.run("INSERT OR REPLACE INTO KeyValue (key, value, update_time) VALUES (?, ?, ?)", [keyIn, valueIn, timestampIn], function (err) {
+                if (err)
+                    reject(err);
+                else
+                    fulfill({ "key": keyIn, "value": valueIn, "timestamp": timestampIn });
+            });
+        });
+    }
+};
+
+module.exports = db;
---
+++ main.js
@@ -0,0 +1,76 @@
+var restify = require('restify');
+var errs = require('restify-errors');
+var db = require('./db.js');
+
+function respond(req, res, next) {
+  res.send('hello ' + req.params.name);
+  next();
+}
+
+var server = restify.createServer();
+
+server.pre(restify.plugins.pre.userAgentConnection());
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
+
+
+// GET method to get value by key and timestamp (optional)
+server.get('/object/:keyIn', function (req, res, next) {
+  var timestampIn = undefined;
+  // Check timestamp exists and is a valid long
+  if (req.query.timestamp) {
+    timestampIn = parseInt(req.query.timestamp);
+    if (isNaN(timestampIn)) {
+      res.send(new errs.InvalidArgumentError(req.query.timestamp + ' is not a valid timestamp'));
+      return next();
+    }
+  }
+
+  // DB action
+  db.getValueByKey(req.params.keyIn, timestampIn).done(function (result) {
+    res.send(result);
+    next();
+  }, function (rejectReason) {
+    res.send(new errs.NotFoundError('No key-value pair found'));
+    next();
+  });
+});
+
+// POST method to insert a new record
+server.post('/object', function (req, res, next) {
+  // Check request body exists
+  if (req.body) {
+    var keys = Object.keys(req.body);
+    // Check request body has exacly one key-value pair
+    if (keys.length === 1) {
+      db.insertUpdateEntry(keys[0], req.body[keys[0]], req.date()).done(function (result) {
+        res.send(result);
+        next();
+      }, function (rejectReason) {
+        console.log(rejectReason);
+        res.send(new errs.InvalidArgumentError(rejectReason));
+        next();
+      });
+    }
+    else {
+      res.send(new errs.InvalidArgumentError('Invalid number of key-value pairs'));
+      next();
+    }
+  }
+  else {
+    res.send(new errs.InvalidArgumentError('Invalid request body'));
+    next();
+  }
+});
+
+server.on('InternalServer', function (req, res, err, cb) {
+  // if you don't want restify to automatically render the Error object
+  // as a JSON response, you can customize the response by setting the
+  // `body` property of the error
+  err.body = 'Sorry but error happens';
+  return cb();
+});
+
+server.listen(8080, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
